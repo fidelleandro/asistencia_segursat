5d283baeb8521643c60c3560789847a7ceb0d240
fidel <fidelleandro@msn.com>
2021-08-23 11:41:14 -0500

    if fixed login layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -186,7 +186,11 @@
                 <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
               </div>
               <div class="info">
+                @guest
+                <div class=""></div>
+                else
                 <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                @endguest
               </div>
             </div>
 
